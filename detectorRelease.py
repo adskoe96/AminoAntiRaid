7536b6db68cbd1abcc040e2704cab81c5c398943
adskoe96 <73867345+adskoe96@users.noreply.github.com>
2021-02-27 16:18:56 +0500

    Add files via upload

--- detectorRelease.py
+++ detectorRelease.py
@@ -1,15 +1,25 @@
 import amino
 import os
 
+#
+#CLIENT, LOGIN, SUBCLIENT
+#
 client = amino.Client()
 client.login(email=os.getenv("email"), password=os.getenv("password"))
 subclient = amino.SubClient(comId=os.getenv("comid"), profile=client.profile)
+whitelist = []
+with open('whitelist.txt', 'r') as file:
+	for whitelistmember in file.readlines():
+		whitelist.append(whitelistmember.strip())
 print('Detector: ON')
 
 def on_message(data):
+#
+#STRINGS
+#
     chatid = data.message.chatId
     nickname = data.message.author.nickname
-    content = data.message.content
+    content = str(data.message.content).split()
     mtype = data.message.type
     mid = data.message.messageId
     uid = data.message.author.userId
@@ -27,6 +37,21 @@ def on_message(data):
 #
     if content == "?hey":
         subclient.send_message(chatId=chatid, message="work status: True")
+#
+#Join
+#
+    if content[0][0] == '!':
+        if content[0][1:].lower() == "join":
+            if any(user in uid for user in whitelist):
+                try:
+                    print(content[-1])
+                    id = client.get_from_code(content[-1]).objectId
+                    subclient.join_chat(id)
+                    subclient.send_message(chatId = chatid, message="Joined")
+                except:
+                    subclient.send_message(chatId = chatid, message="Error")
+            else:
+                subclient.send_message(chatId = chatid, message="You don't have permissions")
 methods = []
 for x in client.callbacks.chat_methods:
     methods.append(client.callbacks.event(client.callbacks.chat_methods[x].__name__)(on_message))
