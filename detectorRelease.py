5ddba4ffc3aa0a6c332c86917abe4c95d3c9dc86
adskoe96 <73867345+adskoe96@users.noreply.github.com>
2021-02-27 12:56:45 +0500

    Add files via upload

---
+++ detectorRelease.py
@@ -0,0 +1,26 @@
+import amino
+import os
+
+client = amino.Client()
+client.login(email=os.getenv("email"), password=os.getenv("password"))
+subclient = amino.SubClient(comId=os.getenv("comid"), profile=client.profile)
+print('Detector: ON')
+
+def on_message(data):
+    chatid = data.message.chatId
+    nickname = data.message.author.nickname
+    content = data.message.content
+    mtype = data.message.type
+    chatname = subclient.get_chat_thread(chatId=data.message.chatId).title
+    mid = data.message.messageId
+    uid = data.message.author.userId
+    print(f'{chatname}: {nickname}: {mtype}: {content}')
+    if (mtype == 100) | (mtype == 109) | (mtype == 107)  | (mtype == 110) | (mtype == 108)  | (mtype == 111) | (mtype == 111):
+        if mtype == 100 and content == None:
+                  pass
+        else:
+            subclient.kick(chatId=chatid, userId = uid, allowRejoin=False)
+            subclient.send_message(chatId = chatid, message = f'MessageType {mtype} detected! Nickname: {nickname} | userId: {uid} | messageId: {mid}.')
+methods = []
+for x in client.callbacks.chat_methods:
+    methods.append(client.callbacks.event(client.callbacks.chat_methods[x].__name__)(on_message))
